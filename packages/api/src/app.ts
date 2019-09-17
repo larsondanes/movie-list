69d48fa3d4c5bbe0cc5604d5166efd515c2ff163
Larson Danes <lgdanes@live.com>
2019-09-17 10:34:48 -0500

    Installed koa-router, created /hello-world endpoint, imported into app.ts

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,6 +1,11 @@
 import * as Koa from "koa";
+import { routes } from "./hello-world";
+
 const app = new Koa();
+
+app.use(routes);
 app.use(async ctx => {
     ctx.body = "Hello World";
 });
+
 app.listen(3000);
